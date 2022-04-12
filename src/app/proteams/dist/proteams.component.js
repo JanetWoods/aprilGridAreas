d80c794803fe8fc40861162c8948bfd09bb97b8b
Janet Woods <janetnwoods@gmail.com>
2022-04-12 11:48:17 -0500

    beginning hamburger for smaller screens

--- src/app/proteams/dist/proteams.component.js
+++ src/app/proteams/dist/proteams.component.js
@@ -18,193 +18,193 @@ var ProteamsComponent = /** @class */ (function () {
                 id: 1,
                 abbr: 'BUF',
                 proTeamName: 'Buffalo Bills',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 2,
                 abbr: 'IND',
                 proTeamName: 'Indianapolis Colts',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 3,
                 abbr: 'MIA',
                 proTeamName: 'Miami Dolphins',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 4,
                 abbr: 'NE',
                 proTeamName: 'New England Patriots',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 5,
                 abbr: 'NYJ',
                 proTeamName: 'New York Jets',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 6,
                 abbr: 'CIN',
                 proTeamName: 'Cincinnati Bengals',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 7,
                 abbr: 'CLE',
                 proTeamName: 'Cleveland Browns',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 8,
                 abbr: 'TEN',
                 proTeamName: 'Tennessee Titans',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 9,
                 abbr: 'PIT',
                 proTeamName: 'Pittsburgh Steelers',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 10,
                 abbr: 'DEN',
                 proTeamName: 'Denver Broncos',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 11,
                 abbr: 'KC',
                 proTeamName: 'Kansas City Chiefs',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 12,
                 abbr: 'LV',
                 proTeamName: 'Las Vegas Raiders',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 13,
                 abbr: 'LAC',
                 proTeamName: 'Los Angeles Chargers',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 14,
                 abbr: 'SEA',
                 proTeamName: 'Seattle Seahawks',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 15,
                 abbr: 'DAL',
                 proTeamName: 'Dallas Cowboys',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 16,
                 abbr: 'NYG',
                 proTeamName: 'New York Giants',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 17,
                 abbr: 'PHI',
                 proTeamName: 'Philadelphia Eagles',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 18,
                 abbr: 'ARI',
                 proTeamName: 'Arizona Cardinals',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 19,
                 abbr: 'WAS',
                 proTeamName: 'Washington Redskins',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 20,
                 abbr: 'CHI',
                 proTeamName: 'Chicago Bears',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 21,
                 abbr: 'DET',
                 proTeamName: 'Detroit Lions',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 22,
                 abbr: 'GB',
                 proTeamName: 'Green Bay Packers',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 23,
                 abbr: 'MIN',
                 proTeamName: 'Minnesota Vikings',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 24,
                 abbr: 'TB',
                 proTeamName: 'Tampa Bay Buccaneers',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 25,
                 abbr: 'ATL',
                 proTeamName: 'Atlanta Falcons',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 26,
                 abbr: 'NAR',
                 proTeamName: 'Los Angeles Rams',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 27,
                 abbr: 'NO',
                 proTeamName: 'New Orleans Saints',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 28,
                 abbr: 'SF',
                 proTeamName: 'San Francisco 49ers',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 29,
                 abbr: 'CAR',
                 proTeamName: 'Carolina Panthers',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 30,
                 abbr: 'JAX',
                 proTeamName: 'Jacksonville Jaguars',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 31,
                 abbr: 'BAL',
                 proTeamName: 'Baltimore Ravens',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
             {
                 id: 32,
                 abbr: 'HOU',
                 proTeamName: 'Houston Texans',
-                picpath: '../../assets/images/HSplayers.jpg'
+                picpath: '../../assets/images/players.jpg'
             },
         ];
     }
